79a549031c247aa428407fd234fdfed0102a5f3a
Jason Meyer <Mars@mars.attlocal.net>
2015-10-30 00:39:26 -0700

    allow new params for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -57,6 +57,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:email, :password, :Firstname, :Lastname, :Username)
     end
 end
