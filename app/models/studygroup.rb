a20c07736a67fd4701c9cd09f183e05e6bfa3b6c
Jason Meyer <Mars@mars.attlocal.net>
2015-10-31 02:21:34 -0700

    allow post for group association

--- app/models/studygroup.rb
+++ app/models/studygroup.rb
@@ -1,4 +1,4 @@
 class Studygroup < ActiveRecord::Base
-  has_many :studygroups_users
+  has_many :studygroups_users, :dependent => :destroy
   has_many :users, :through => :studygroups_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :studygroups_users
+  has_many :studygroups_users, :dependent => :destroy
   has_many :studygroups, :through => :studygroups_users
   validates_uniqueness_of :email
 end
